ead73f4e47b5c957fd2b49ed04f73a0e789600c1
Amir Hamza <hamza.com.bd@gmail.com>
2024-03-04 00:22:56 +0600

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
+import { EditSaleComponent } from './components/sale/edit-sale/edit-sale.component';
+import { SaleListComponent } from './components/sale/sale-list/sale-list.component';
+import { SaleDetailsComponent } from './components/sale/sale-details/sale-details.component';
+import { EditProductComponent } from './components/product/edit-product/edit-product.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
 import { CustomerListComponent } from './components/customer/customer-list/customer-list.component';
 import { EditCustomerComponent } from './components/customer/edit-customer/edit-customer.component';
 import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
-import { EditProductComponent } from './components/product/edit-product/edit-product.component';
-import { SaleListComponent } from './components/sale/sale-list/sale-list.component';
-import { SaleDetailsComponent } from './components/sale/sale-details/sale-details.component';
-import { EditSaleComponent } from './components/sale/edit-sale/edit-sale.component';
-import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
 
 const routes: Routes = [
   {
