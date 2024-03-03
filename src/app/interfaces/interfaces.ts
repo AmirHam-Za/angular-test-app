d13e6499050ccfdae39173cbda9fa94d33122869
Amir Hamza <hamza.com.bd@gmail.com>
2024-03-03 23:48:27 +0600

    updated

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,23 +1,4 @@
-export interface Post {
-  "id": string,
-  "userId": string,
-  "picture": string,
-  "title": string,
-  "content": string
-}
 
-export interface User {
-  "id": string,
-  "title": string,
-  "isActive": boolean,
-  "picture": string,
-  "age": string,
-  "gender": string,
-  "company": string,
-  "email": string,
-  "phone": string,
-  "address": string
-}
 export interface Customer {
   "id": string,
   "first_name": string,
